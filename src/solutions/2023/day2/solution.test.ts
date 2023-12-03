d026cdfdafc345a4b1d176e80770c6fe8a3c5727
Gediminas Petrikas <gediminas.petrikas@carvertical.com>
2023-12-03 18:52:42 +0200

    refactor: day2

--- src/solutions/2023/day2/solution.test.ts
+++ src/solutions/2023/day2/solution.test.ts
@@ -6,14 +6,6 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
 
-// const input2 = `two1nine
-// eightwothree
-// abcone2threexyz
-// xtwone3four
-// 4nineeightseven2
-// zoneight234
-// 7pqrstsixteen`;
-
 describe('day2', () => {
   describe('first part', () => {
     it('returns correct answer', async () => {
--- src/solutions/2023/day2/solution.ts
+++ src/solutions/2023/day2/solution.ts
@@ -1,19 +1,23 @@
 import { readFile } from 'fs/promises';
-import _ from 'lodash';
 
-const maxCubeCount: Record<string, number> = {
+type Cube = {
+  count: number;
+  color: string;
+};
+
+export const maxCubeCount: Record<string, number> = {
   red: 12,
   green: 13,
   blue: 14,
 };
 
-enum Color {
+export enum Color {
   red = 'red',
   green = 'green',
   blue = 'blue',
 }
 
-const solve =
+export const solve =
   (solveFn: (input: string) => Promise<number>) =>
   async (inputString?: string): Promise<number> => {
     const input =
@@ -23,8 +27,13 @@ const solve =
     return solveFn(input);
   };
 
+const parseCubes = (game: string): Cube[] =>
+  game.match(/(\d+) (\w+)/g)!.map((cube) => {
+    const [count, color] = cube.split(' ');
+    return { count: parseInt(count), color };
+  });
+
 const getFirstPartAnswer = async (input: string) => {
-  console.log('first part');
   const lines = input.split('\n');
 
   const answer = lines.reduce((acc, line) => {
@@ -32,11 +41,7 @@ const getFirstPartAnswer = async (input: string) => {
     const gameNumber = parseInt(gameNumberString.match(/Game (\d+)/)![1]);
 
     const isPossible = gameData.split(';').every((game) => {
-      const cubes = game.match(/(\d+) (\w+)/g)!.map((cube) => {
-        const [count, color] = cube.split(' ');
-        return { count: parseInt(count), color };
-      });
-
+      const cubes = parseCubes(game);
       return cubes.every((cube) => cube.count <= maxCubeCount[cube.color]);
     });
     return isPossible ? acc + gameNumber : acc;
@@ -46,7 +51,6 @@ const getFirstPartAnswer = async (input: string) => {
 };
 
 const getSecondPartAnswer = async (input: string) => {
-  console.log('second part');
   const lines = input.split('\n');
 
   const answer = lines.reduce((acc, line) => {
@@ -54,14 +58,12 @@ const getSecondPartAnswer = async (input: string) => {
 
     const maxCubeCount = gameData.split(';').reduce(
       (acc, draw) => {
-        const cubes: { color: Color; count: number }[] = draw
-          .match(/(\d+) (\w+)/g)!
-          .map((cube) => {
-            const [count, color] = cube.split(' ');
-            return { count: parseInt(count), color: color as Color };
-          });
+        const cubes = parseCubes(draw).map((cube) => ({
+          count: cube.count,
+          color: cube.color as Color,
+        }));
 
-        const getMax = (accColor: number, color: Color): number =>
+        const getMax = (accColor: number, color: Color) =>
           Math.max(
             accColor,
             cubes.find((cube) => cube.color === color)?.count || 0,
